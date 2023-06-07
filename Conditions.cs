a9464fe042c9cf28553bc518ec622ff561d6d7c1
Randy <131615165+meet-randy@users.noreply.github.com>
2023-06-07 18:03:57 +0200

    Create Conditions.cs

---
+++ Conditions.cs
@@ -0,0 +1,41 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
+        
+        switch (N % 2)
+        {
+            case 0:
+                if (N >= 2 && N <= 5)
+                    result = "Not Weird";
+                else if (N >= 6 && N <= 20)
+                    result = "Weird";
+                else
+                    result = "Not Weird";
+                break;
+            default:
+                result = "Weird";
+                break;
+        }
+
+            Console.WriteLine(result);
+        }
+    }
+}
